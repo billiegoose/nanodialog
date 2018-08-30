4d51c03fd70b65a47d1bab148b265fecbe4834b6
William Hilton <wmhilton@gmail.com>
2018-08-29 20:04:00 -0400

    optimize things a bit

--- index.mjs
+++ index.mjs
@@ -1,94 +1,84 @@
+const defaultCSS = {
+  formCSS: "display: flex; flex-direction: column;",
+  inputCSS: "padding: 5px; margin-bottom: 1em;",
+  buttonrowCSS: "display: flex; justify-content: flex-end",
+  buttonCSS: "margin-left: 0.5em",
+  pCSS: "",
+}
+
+function createDialog(innerHTML, onClose, onCancel) {
+  let el = document.createElement("dialog");
+  el.innerHTML = innerHTML
+  document.body.appendChild(el);
+  el.showModal();
+  el.addEventListener("close", function() {
+    onClose.apply(this);
+    this.remove();
+  });
+  el.addEventListener("cancel", function() {
+    onCancel.apply(this);
+    this.remove();
+  });
+  return el
+}
+
+const h = (type, title, style, attr) =>
+  type === 'p' ? `<p style="${style||''}" ${attr||''}>${title}</p>`
+    : `<input type="${type}" name="${title}" ${
+        type==='submit' ? `value="${title}"` : `placeholder="${title}"`
+      } style="${style||''}" ${attr||''}>`
+
 export function loginDialog(title, opts = {}) {
   return new Promise((resolve, reject) => {
-    let _opts = Object.assign({
-      formCSS: "display: flex; flex-direction: column;",
-      inputCSS: "padding: 5px; margin-bottom: 1em;",
-      buttonrowCSS:
-        "display: flex; flex-direction: row-reverse; justify-content: flex-start"
-    });
-    let favDialog = document.createElement("dialog");
-    favDialog.innerHTML = `
+    let _opts = Object.assign({}, defaultCSS, opts);
+    let favDialog = createDialog(`
       <form method="dialog" style="${_opts.formCSS}">
-        <p>${title}</p>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          style="${_opts.inputCSS}"
-          autofocus>
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          style="${_opts.inputCSS}">
+        ${h('p', title, _opts.pCSS)}
+        ${h('text', 'Username', _opts.inputCSS, 'autofocus')}
+        ${h('password', 'Password', _opts.inputCSS)}
         <menu style="${_opts.buttonrowCSS}">
-          <button
-            type="submit"
-            value="submit">
-            Submit
-          </button>
-          <button
-            type="submit"
-            value="cancel">
-            Cancel
-          </button>
+          ${h('submit', 'Submit', _opts.buttonCSS)}
+          ${h('submit', 'Cancel', _opts.buttonCSS)}
         </menu>
-      </form>`;
-    document.body.appendChild(favDialog);
-    favDialog.showModal();
-    favDialog.addEventListener("close", function(event) {
-      console.log(this.returnValue);
-      if (this.returnValue === "cancel" || this.returnValue === "") {
-        reject("cancel");
-      } else {
-        let data = {};
-        let form = new FormData(this.querySelector("form"));
-        for (let [key, value] of form.entries()) {
-          data[key] = value;
+      </form>`,
+      function(event) {
+        let r = this.returnValue
+        if (r === "Cancel") {
+          resolve(null);
+        } else {
+          let data = {};
+          let form = new FormData(this.querySelector("form"));
+          for (let [key, value] of form.entries()) {
+            data[key] = value;
+          }
+          resolve(data);
         }
-        resolve(data);
-      }
-      this.remove();
-    });
+      },
+      () => resolve(null)
+    );
   });
 }
 
 export function confirmDialog(title, opts = {}) {
   return new Promise((resolve, reject) => {
-    let _opts = Object.assign({
-      formCSS: "display: flex; flex-direction: column;",
-      inputCSS: "padding: 5px; margin-bottom: 1em;",
-      buttonrowCSS:
-        "display: flex; flex-direction: row-reverse; justify-content: flex-start"
-    });
-    let favDialog = document.createElement("dialog");
-    favDialog.innerHTML = `
+    let _opts = Object.assign({}, defaultCSS, opts);
+    let favDialog = createDialog(`
       <form method="dialog" style="${_opts.formCSS}">
-        <p>${title}</p>
+        ${h('p', title, _opts.pCSS)}
         <menu style="${_opts.buttonrowCSS}">
-          <button
-            type="submit"
-            value="yes"
-            autofocus>
-            Yes
-          </button>
-          <button
-            type="submit"
-            value="no">
-            No
-          </button>
+          ${h('submit', 'Yes', _opts.buttonCSS, 'autofocus')}
+          ${h('submit', 'No', _opts.buttonCSS)}
         </menu>
-      </form>`;
-    document.body.appendChild(favDialog);
-    favDialog.showModal();
-    favDialog.addEventListener("close", function(event) {
-      console.log(this.returnValue);
-      if (this.returnValue === "no") {
-        resolve(false);
-      } else if (this.returnValue === "yes") {
-        resolve(true);
-      }
-      this.remove();
-    });
+      </form>`,
+      function(event) {
+        let r = this.returnValue
+        if (r === "No") {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      },
+      () => resolve(null)
+    );
   });
 }
